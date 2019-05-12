4720fe1d065e597127cf9130e757212ce2ab7306
Tess <tessneau@gmail.com>
2019-05-12 17:21:21 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
 def alphabetize(arr)
-  # code here
+
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_hash = {}
+
+  esperanto_alphabet.split(//).each_with_index do |letter, index|
+      esp_hash[letter] = index
+  end
+  new_arr = []
+  arr.sort_by do |phrase|
+    phrase.split(%r{\s*}).map do |letter|
+      esp_hash[letter]
+    end
+  end
 end
